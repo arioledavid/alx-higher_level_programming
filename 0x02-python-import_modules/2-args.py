d59102f83154a80ba4cf5a6b4e47245f591c2626
Ariole David <davidariole@gmail.com>
2021-09-09 15:02:30 +0000

    updated task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
     num = len(sys.argv)
-    for i in range(1, len(sys.argv))
+    for i in range(1, len(sys.argv)):
         if num == 1:
             print("{} arguments.".format(num - 1))
         elif num == 2:
