f7c9fb6d0d025500013fb8136907286e90107676
arioledavid <davidariole@gmail.com>
2021-09-11 18:16:56 -0700

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -4,7 +4,7 @@ import hidden_4
 def discovr():
     name = dir(hidden_4)
     for i in name:
-        if i[:2] != '__':
+        if i[0:2] != '__':
             print("{:s}".format(i))
 
 if __name__ == "__main__":
