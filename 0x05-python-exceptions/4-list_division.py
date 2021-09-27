70ece472bbf0825f692a5b9a78371a8a81ce45de
arioledavid <davidariole@gmail.com>
2021-09-27 16:58:52 -0700

    Task 5

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
-    idx = 0 
+    idx = 0
     if list_length <= 0:
         print("out of range")
         return new_list
