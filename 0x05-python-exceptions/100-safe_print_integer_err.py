91884df1fba03f6d1557c4272d9655c7d4819da9
arioledavid <davidariole@gmail.com>
2021-09-27 17:34:23 -0700

    Task 6 and 7

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".formaat(value))
+        return True
+    except ValueError as err:
+        print("Exception: {}".format(err), file=sys.stderr)
+        return False
+    except TypeError as err:
+        print("Exception: {}".format(err), file=sys.stderr)
+        return False
