e59b01f3ba04d0c5daebca91a4c25c2cebab3c92
Ariole David <davidariole@gmail.com>
2021-11-18 22:29:35 +0100

    Task 7 and 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- This script lists all records of the table second_table 
 -- off the database hbtn_0c_0
-SELECT `score`, `name` FROM second_table WHERE `name' IS NOT NULL ORDER BY `score` DESC;
+SELECT `score`, name FROM second_table WHERE name IS NOT NULL AND name != '' ORDER BY score DESC;
